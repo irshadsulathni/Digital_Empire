5c23d06180e9ec0fbf24f22153cf47f7ea5e2d15
irshadsulthani <irshadpallar2020@gmail.com>
2024-08-10 09:11:41 +0530

    nothing

--- controllers/userController.js
+++ controllers/userController.js
@@ -628,6 +628,7 @@ const loadHowShop = async (req, res) => {
     }
 }
 
+
 const passwordUpdate = async (req, res) => {
     try {
         const { currentPassword, newPassword, confirmPassword } = req.body;
